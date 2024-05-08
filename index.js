d26bd12b7f3dfb965a5552b846562e177bebdd2f
Voichita Maria <sabo_voichita@yahoo.com>
2024-05-08 14:11:47 +0200

    76.json

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function loadLesson(lessonNumber) {
 }
 
 function initEvents() {
-  const numberOfLessons = 75;
+  const numberOfLessons = 76;
   renderDivs(numberOfLessons);
 
   for (let i = 1; i <= numberOfLessons; i++) {
